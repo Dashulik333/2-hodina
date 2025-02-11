6a272b14c73b616820ab390585e5cdc062ddcdb3
Dashulik333 <radhenkodasha+@gmail.com>
2025-02-11 15:23:58 +0100

    2 hodina

---
+++ main.py
@@ -0,0 +1,12 @@
+print("hello world")
+
+print("hello world", 15 )   #Pokud chci více datových typu
+
+#Vytvoření proměnné (deklarace/inicializace)
+
+
+cislo = 1 #Do proměnné císlo je uložná hodnota 1:
+
+
+print("vstup", cislo)
+ 
